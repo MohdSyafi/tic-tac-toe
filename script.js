44915e787ccf0885d351dd333f80e0afadd00129
syafi <syafimohamad13@gmail.com>
2022-09-24 22:52:42 +0800

    add function to display end dialog

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const Player = (name,symbol,selectedTiles) => {
 
   const getNameToDisplay =()=> name + ": " +  symbol;
+  const getPlayerName =()=> name;
+  const clearMoves =()=> selectedTiles = [];
   const getSymbol =()=> symbol;
   const winCombinations = [
-    ["1-1","2-1","2-1"],
+    ["1-1","2-1","3-1"],
     ["1-2","2-2","3-2"],
     ["1-3","2-3","3-3"],
     ["1-1","1-2","1-3"],
@@ -21,8 +23,9 @@ const Player = (name,symbol,selectedTiles) => {
     return selectedTiles;
   };
 
-  const checkWin = () =>{     
+  const win = () =>{     
     let count = 0;
+    let result = false;
 
     winCombinations.forEach(item=>{
       count = 0;
@@ -34,12 +37,15 @@ const Player = (name,symbol,selectedTiles) => {
       });
 
       if(count == 3){
-        return true;
+        result = true;       
       }
 
-    })
+    });
 
-    return false;
+    if(result)
+      return true;
+    else
+      return false;
   }
 
   return {
@@ -47,7 +53,9 @@ const Player = (name,symbol,selectedTiles) => {
     getSymbol,
     addMove,
     getMoves,
-    checkWin
+    getPlayerName,
+    win,
+    clearMoves
   };
 
 };
@@ -79,27 +87,24 @@ const Round = (() => {
 const gameBoard = ((doc) => {
 
     const startDialog = doc.querySelector(".start-dialog");
+    const endDialog = doc.querySelector(".end-dialog");
     let player1 = Player();
     let player2 = Player();
    
-    const start = () =>{    
-      startDialog.style.display = 'block';
-      const form = doc.querySelector("form");
+    const start = () =>{
 
+      startDialog.style.display = 'block';
+      doc.querySelector(".player1name").value = "";
+      doc.querySelector(".player2name").value = "";
+
+      const form = doc.querySelector("form");      
       form.addEventListener("submit", e=>{
         e.preventDefault();
 
         player1 = Player(form.elements['player1name'].value , "X",[]);
         player2 = Player(form.elements['player2name'].value, "O",[]);
 
-        doc.querySelector(".first-player").innerHTML = player1.getNameToDisplay();
-        doc.querySelector(".second-player").innerHTML = player2.getNameToDisplay();
-
         closeStartDialog();
-
-        Round.startRound();
-        let x = player1.getSymbol();
-        Round.setcurrentMove(x); 
         setupBoard();  
       })
     };
@@ -109,11 +114,22 @@ const gameBoard = ((doc) => {
     }
 
     const setupBoard = ()=>{
+      
+      doc.querySelector(".first-player").innerHTML = player1.getNameToDisplay();
+      doc.querySelector(".second-player").innerHTML = player2.getNameToDisplay();
+
+      player1.clearMoves();
+      player2.clearMoves();
+
+      Round.startRound();
+      Round.setcurrentMove(player1.getSymbol()); 
+
       const tiles = doc.querySelectorAll(".tile");
       tiles.forEach(item=>{ 
         item.innerHTML= "";
         item.addEventListener("click",e => updateTile(item,item.dataset.position));        
       });
+
     }
 
     const updateTile  = (item,position) => {
@@ -125,19 +141,42 @@ const gameBoard = ((doc) => {
           item.innerHTML = player1.getSymbol();
           player1.addMove(position);
           Round.setcurrentMove(player2.getSymbol());
-          player1.checkWin();
+          
+          if(player1.win()){
+            endGame(player1.getPlayerName());
+          }
   
         }else if(Round.getcurrentMove() == player2.getSymbol()){
   
           item.innerHTML = player2.getSymbol();
           player2.addMove(position);
           Round.setcurrentMove(player1.getSymbol());
-          player2.checkWin();
+
+          if(player2.win()){
+            endGame(player2.getPlayerName());
+          }
         }
 
       }
      
     };
+
+    const endGame = (winner)=>{
+
+      endDialog.style.display = "block";
+      const displayWinner = doc.querySelector(".end-dialog-content p");
+      displayWinner.innerHTML = "You win " + winner +" !!";
+
+      endDialog.querySelector(".replayBtn").addEventListener("click",e => {
+        endDialog.style.display = "none";
+        setupBoard()
+      });
+
+      endDialog.querySelector(".newGameBtn").addEventListener("click",e => {
+        endDialog.style.display = "none";
+        start()
+      });
+    }
 
     return {
         start,
