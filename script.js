03bd5c25fbe2bb1330f2fe5b9a486ee980e71592
syafi <syafimohamad13@gmail.com>
2022-09-24 15:47:33 +0800

    Add function to update tile     - limit round count to 9     - forbid user clicking on updated tiles

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-const game = ((doc) => {
+const Player = (name,symbol) => {
 
-    let player1name = doc.querySelector(".first-player");
-    let player2name = doc.querySelector(".second-player");
+  const getNameToDisplay =()=> name + ": " +  symbol;
+  const getSymbol =()=> symbol;
+
+  return {
+    getNameToDisplay,
+    getSymbol
+  };
+
+};
+
+const Round = (() => {
+  let noOfRound = 0;
+  let currentMove = " ";
+
+  const getnoOfRound =()=> noOfRound;
+ 
+  const increaseRoundNo =()=> noOfRound++;
+  const startRound =()=> noOfRound = 0;
+
+  function getcurrentMove (){ return this.currentMove};
+  function setcurrentMove(input){
+    this.currentMove = input;
+    increaseRoundNo();
+  };
+
+  return {
+    getnoOfRound,
+    getcurrentMove,
+    setcurrentMove,
+    startRound
+  };
+
+})();
+
+const gameBoard = ((doc) => {
+
     const startDialog = doc.querySelector(".start-dialog");
-    const tileGrid = doc.querySelector(".tile-grid");
+    let player1 = Player();
+    let player2 = Player();
+   
 
     const start = () =>{    
       startDialog.style.display = 'block';
@@ -11,29 +47,60 @@ const game = ((doc) => {
 
       form.addEventListener("submit", e=>{
         e.preventDefault();
-        player1name.textContent =  form.elements['player1name'].value;
-        player2name.textContent =  form.elements['player2name'].value;
+
+        player1 = Player(form.elements['player1name'].value , "X");
+        player2 = Player(form.elements['player2name'].value, "O");
+
+        doc.querySelector(".first-player").innerHTML = player1.getNameToDisplay();
+        doc.querySelector(".second-player").innerHTML = player2.getNameToDisplay();
+
         closeStartDialog();
+
+        Round.startRound();
+        let x = player1.getSymbol();
+        Round.setcurrentMove(x); 
+        setupBoard();  
       })
-
-      setupBoard();
     };
-
-    const setupBoard = ()=>{
-        const tiles = doc.querySelectorAll(".tile");
-        tiles.forEach(item=>{ item.innerHTML= ""});
-    }
 
     const closeStartDialog = () =>{
         startDialog.style.display = "none";
     }
 
+    const setupBoard = ()=>{
+      const tiles = doc.querySelectorAll(".tile");
+      tiles.forEach(item=>{ 
+        item.innerHTML= "";
+        item.addEventListener("click",e => updateTile(item));        
+      });
+    }
+
+    const updateTile  = (item) => {
+      
+      if(Round.getnoOfRound()<=9 && item.innerHTML ==""){
+
+        if(Round.getcurrentMove() == player1.getSymbol()){
+
+          item.innerHTML = player1.getSymbol();
+          Round.setcurrentMove(player2.getSymbol());
+  
+        }else if(Round.getcurrentMove() == player2.getSymbol()){
+  
+          item.innerHTML = player2.getSymbol();
+          Round.setcurrentMove(player1.getSymbol());
+  
+        }
+
+      }
+
+      
+    };
+
     return {
-        start
+        start,
     };
 
   })(document);
 
-
-  game.start();
+  gameBoard.start();
 
