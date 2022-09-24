fda4b21f679f3ed5653c5e632b3e6a8d774db38b
syafi <syafimohamad13@gmail.com>
2022-09-24 17:13:05 +0800

    Add function to check win combination

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-const Player = (name,symbol) => {
+const Player = (name,symbol,selectedTiles) => {
 
   const getNameToDisplay =()=> name + ": " +  symbol;
   const getSymbol =()=> symbol;
+  const winCombinations = [
+    ["1-1","2-1","2-1"],
+    ["1-2","2-2","3-2"],
+    ["1-3","2-3","3-3"],
+    ["1-1","1-2","1-3"],
+    ["2-1","2-2","2-3"],
+    ["3-1","3-2","3-3"],
+    ["1-1","2-2","3-3"],
+    ["3-1","2-2","1-3"],
+  ]
+  
+  function addMove(input) {
+    selectedTiles.push(input);
+  };
+
+  function getMoves() {
+    return selectedTiles;
+  };
+
+  const checkWin = () =>{     
+    let count = 0;
+
+    winCombinations.forEach(item=>{
+      count = 0;
+
+      item.forEach(position=>{  
+        if(selectedTiles.includes(position)){
+          count++;
+        }
+      });
+
+      if(count == 3){
+        return true;
+      }
+
+    })
+
+    return false;
+  }
 
   return {
     getNameToDisplay,
-    getSymbol
+    getSymbol,
+    addMove,
+    getMoves,
+    checkWin
   };
 
 };
@@ -40,7 +82,6 @@ const gameBoard = ((doc) => {
     let player1 = Player();
     let player2 = Player();
    
-
     const start = () =>{    
       startDialog.style.display = 'block';
       const form = doc.querySelector("form");
@@ -48,8 +89,8 @@ const gameBoard = ((doc) => {
       form.addEventListener("submit", e=>{
         e.preventDefault();
 
-        player1 = Player(form.elements['player1name'].value , "X");
-        player2 = Player(form.elements['player2name'].value, "O");
+        player1 = Player(form.elements['player1name'].value , "X",[]);
+        player2 = Player(form.elements['player2name'].value, "O",[]);
 
         doc.querySelector(".first-player").innerHTML = player1.getNameToDisplay();
         doc.querySelector(".second-player").innerHTML = player2.getNameToDisplay();
@@ -71,29 +112,31 @@ const gameBoard = ((doc) => {
       const tiles = doc.querySelectorAll(".tile");
       tiles.forEach(item=>{ 
         item.innerHTML= "";
-        item.addEventListener("click",e => updateTile(item));        
+        item.addEventListener("click",e => updateTile(item,item.dataset.position));        
       });
     }
 
-    const updateTile  = (item) => {
+    const updateTile  = (item,position) => {
       
       if(Round.getnoOfRound()<=9 && item.innerHTML ==""){
 
         if(Round.getcurrentMove() == player1.getSymbol()){
 
           item.innerHTML = player1.getSymbol();
+          player1.addMove(position);
           Round.setcurrentMove(player2.getSymbol());
+          player1.checkWin();
   
         }else if(Round.getcurrentMove() == player2.getSymbol()){
   
           item.innerHTML = player2.getSymbol();
+          player2.addMove(position);
           Round.setcurrentMove(player1.getSymbol());
-  
+          player2.checkWin();
         }
 
       }
-
-      
+     
     };
 
     return {
